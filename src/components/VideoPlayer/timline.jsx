3092f809328c4ca7d4be1c0e848c5e059320c1dc
Atiksh Srivastava <atiksh.srivastava@cars24.com>
2024-11-09 22:33:10 +0530

    added timeline changes

--- src/components/VideoPlayer/timline.jsx
+++ src/components/VideoPlayer/timline.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState, useMemo } from "react";
 import { formatTime } from "../../utils";
 import RightArrow from "./images/forward.svg";
 import PropTypes from "prop-types";
@@ -13,7 +13,6 @@ export default function Timeline({
   const timelineRef = useRef(null);
   const [draggingBlock, setDraggingBlock] = useState(null);
   const [dragType, setDragType] = useState(null); // 'start', 'end', or 'move'
-  const totalMinutes = Math.ceil(duration / 60);
 
   const handleTimelineClick = (e) => {
     if (timelineRef.current && !draggingBlock) {
@@ -63,67 +62,98 @@ export default function Timeline({
     setDragType(null);
   };
 
-  return (
-    <div
-      className="relative"
-      ref={timelineRef}
-      onClick={handleTimelineClick}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp}
-    >
-      <div className="w-full h-24 bg-gray-200 rounded relative overflow-hidden">
-        <div
-          className="absolute top-0 left-0 h-full bg-blue-500 opacity-30"
-          style={{ width: `${(currentTime / duration) * 100}%` }}
-        ></div>
+  const timeIntervals = useMemo(() => {
+    const intervals = [0];
+    const numberOfIntervals = 4;
+    for (let i = 1; i < numberOfIntervals; i++) {
+      intervals.push((i / numberOfIntervals) * duration);
+    }
+    intervals.push(duration);
+    return intervals;
+  }, [duration]);
 
-        {zoomBlocks.map((block) => (
+  return (
+    <>
+      <div className="text-black mb-4 mt-4">
+        {formatTime(currentTime)} / {formatTime(duration)}
+      </div>
+      <div
+        className="relative"
+        ref={timelineRef}
+        onClick={handleTimelineClick}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+      >
+        <div className="w-full h-24 bg-gray-200 rounded relative overflow-hidden">
           <div
-            key={block.id}
-            className="absolute top-0 h-full bg-green-500 opacity-50 cursor-move rounded-l-md rounded-r-md"
-            style={{
-              left: `${(block.startTime / duration) * 100}%`,
-              width: `${((block.endTime - block.startTime) / duration) * 100}%`,
-            }}
-            onMouseDown={(e) => handleMouseDown(e, block, "move")}
-          >
-            <div
-              className="absolute left-0 top-0 w-2 h-full bg-yellow-500 cursor-w-resize flex items-center rounded-l-md select-none"
-              onMouseDown={(e) => {
-                handleMouseDown(e, block, "start");
-              }}
-            >
-              <img src={RightArrow} alt="left" className="h-2 w-2" />
-            </div>
-            <div
-              className="absolute right-0 top-0 w-2 h-full bg-yellow-500 cursor-e-resize flex items-center rounded-r-md select-none"
-              onMouseDown={(e) => {
-                handleMouseDown(e, block, "end");
-              }}
-            >
-              <img src={RightArrow} alt="left" className="h-2 w-2 rotate-180" />
-            </div>
-          </div>
-        ))}
-        <div
-          className="absolute top-0 w-0.5 h-full bg-red-500"
-          style={{ left: `${(currentTime / duration) * 100}%` }}
-        ></div>
-        {Array.from({ length: Math.ceil(duration) }).map((_, index) => (
-          <div
-            key={index}
-            className="absolute top-0 w-px h-2 bg-gray-400"
-            style={{ left: `${(index / duration) * 100}%` }}
+            className="absolute top-0 left-0 h-full bg-blue-500 opacity-30"
+            style={{ width: `${(currentTime / duration) * 100}%` }}
           ></div>
-        ))}
+
+          {zoomBlocks.map((block) => (
+            <div
+              key={block.id}
+              className="absolute top-0 h-full bg-green-500 opacity-50 cursor-move rounded-l-md rounded-r-md"
+              style={{
+                left: `${(block.startTime / duration) * 100}%`,
+                width: `${
+                  ((block.endTime - block.startTime) / duration) * 100
+                }%`,
+              }}
+              onMouseDown={(e) => handleMouseDown(e, block, "move")}
+            >
+              <div
+                className="absolute left-0 top-0 w-2 h-full bg-yellow-500 cursor-w-resize flex items-center rounded-l-md select-none"
+                onMouseDown={(e) => {
+                  handleMouseDown(e, block, "start");
+                }}
+              >
+                <img src={RightArrow} alt="left" className="h-2 w-2" />
+              </div>
+              <div
+                className="absolute right-0 top-0 w-2 h-full bg-yellow-500 cursor-e-resize flex items-center rounded-r-md select-none"
+                onMouseDown={(e) => {
+                  handleMouseDown(e, block, "end");
+                }}
+              >
+                <img
+                  src={RightArrow}
+                  alt="right"
+                  className="h-2 w-2 rotate-180"
+                />
+              </div>
+            </div>
+          ))}
+          <div
+            className="absolute top-0 w-0.5 h-full bg-red-500"
+            style={{ left: `${(currentTime / duration) * 100}%` }}
+          ></div>
+          {timeIntervals.map((time, index) => (
+            <div
+              key={index}
+              className="absolute top-0 w-px h-2 bg-gray-400"
+              style={{ left: `${(time / duration) * 100}%` }}
+            ></div>
+          ))}
+        </div>
+        <div className="mt-2 text-sm text-gray-600 flex justify-between relative">
+          {timeIntervals.map((time, index) => (
+            <React.Fragment key={index}>
+              <div
+                className="absolute"
+                style={{ left: `${(time / duration) * 100}%`, transform: 'translateX(-50%)' }}
+              >
+                <span>{formatTime(time)}</span>
+              </div>
+              {index === timeIntervals.length - 1 && (
+                <div className="flex-grow" />
+              )}
+            </React.Fragment>
+          ))}
+        </div>
       </div>
-      <div className="mt-2 text-sm text-gray-600 flex justify-between">
-        {Array.from({ length: totalMinutes + 1 }).map((_, index) => (
-          <span key={index}>{formatTime(index * 60)}</span>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
@@ -131,6 +161,6 @@ Timeline.propTypes = {
   onBlockUpdate: PropTypes.func,
   duration: PropTypes.number,
   currentTime: PropTypes.number,
-  zoomBlocks: PropTypes.func,
+  zoomBlocks: PropTypes.array,
   onTimelineClick: PropTypes.func,
 };
